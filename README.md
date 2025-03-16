e064ef45d20c0f304c4ae7c4060cf9b079618636
Le Khanh Toan <toanlk@bestprice.vn>
2025-03-16 08:52:33 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # docker-browser
+Browser for queue jobs
+
+Access instance
+```
+docker run -it docker-browser /bin/bash
+```
---
+++ build.sh
@@ -0,0 +1,6 @@
+clear
+
+docker build -t docker-browser .
+
+# docker tag docker-browser toanlk/browser:latest
+# docker push toanlk/browser:latest
